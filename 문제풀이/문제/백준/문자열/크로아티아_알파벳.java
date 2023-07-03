b752507c73d7da01232ba866a1155581e2de827d
MeteorLee <meteorproject529@gmail.com>
2023-07-03 09:35:52 +0900

    study: 문자열

---
+++ 문제풀이/문제/백준/문자열/크로아티아_알파벳.java
@@ -0,0 +1,34 @@
+package 문제.백준.문자열;
+
+import java.util.Scanner;
+
+public class 크로아티아_알파벳 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int count = 0;
+        int replaceCount = 0;
+
+        String[] alphabets = new String[] {
+                "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="
+        };
+        String str = sc.nextLine();
+
+        for (int i = 0; i < alphabets.length; i++) {
+            replaceCount += countAlphabet(str, alphabets[i]);
+        }
+
+        count = replaceCount + (str.length() - replaceCount * 2);
+        System.out.println(count);
+
+    }
+
+    private static int countAlphabet(String str, String alphabet) {
+        String replacedStr = str.replace(alphabet, "");
+
+        int count = (str.length() - replacedStr.length()) / alphabet.length();
+
+        return count;
+
+    }
+}
